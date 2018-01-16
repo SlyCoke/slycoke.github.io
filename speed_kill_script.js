079d5862a444582bf5b318bf17558a317061cd04
SlyCoke <31125890+SlyCoke@users.noreply.github.com>
2018-01-16 00:00:52 -0500

    Add files via upload

---
+++ speed_kill_script.js
@@ -0,0 +1,15 @@
+ $(document).ready(function(){
+ 	
+	//var interval = 1000 * 60 * 0.5; // where X is your every X minutes
+	//	setInterval(ajax_call, interval);
+	getTwitchUsersSheets();
+getEvents();
+getBossKC();
+getSpeedKillTimes();	
+document.getElementById("defaultOpen").click();
+
+// Get a reference to the database service
+
+// getSpeedKillTimes();
+
+});
